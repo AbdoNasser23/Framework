92b03d4ab7496e4ade7fbb389a0f7cfc7ac4d8a8
Abdo Nasser <bn645370@gmail.com>
2026-07-02 12:39:39 +0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mvc/framwork',
+        'pretty_version' => '1.0.0',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mvc/framwork' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mvcphp/framwork' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => 'ff4bffeb52b01e9e77d6a9647f17da953cbc6c1e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mvcphp/framwork',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
